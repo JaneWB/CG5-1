42a5cbc88183063a50fb4ac7a7d2868736c57e7d
JaneWB <brownjannick@att.net>
2018-08-09 15:40:57 -0500

    Add project files.

---
+++ CG5-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
