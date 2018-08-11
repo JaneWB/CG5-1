b5106622f4156d5111f471da6b5550525e7c504a
JaneWB <brownjannick@att.net>
2018-08-10 22:58:50 -0500

    First and last name with a method

--- CG5-1/Program.cs
+++ CG5-1/Program.cs
@@ -6,7 +6,38 @@ namespace CG5_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a program that asks the user to enter their first name. Then have it enter their last name.
+            //Create a method that combines the first and last name and returns it as a new value. Print the new value.
+            //Upload your program to a new repository called CG 5 - 1.
+            //Upload a link to your repository here when you are finished.
+
+            Console.WriteLine("What is your first name?");
+            string myFirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string myLastName = Console.ReadLine();
+
+            //string myName = fullName(myFirstName, myLastName);
+            //Console.WriteLine("Hello " + myName);
+            //or
+            Console.WriteLine("Hello " + fullName(myFirstName, myLastName));
+            Console.ReadLine();
+
+
+
         }
+        /// <summary>
+        /// First and last name.
+        /// </summary>
+        /// <param name="myFirstName"></param>
+        /// <param name="myLastName"></param>
+        /// <returns>First and name</returns>
+        private static string fullName(string myFirstName, string myLastName)
+        {
+            return (myFirstName + " " + myLastName);
+        }
+                    
+          
+      
     }
 }
